b01546b9e6bd275dce2d41d84558cffc012547ed
ElChicoEevee <kirbyyeray@hotmail.com>
2025-03-16 11:25:14 +0100

    Update snapmatic-converter.py

--- snapmatic-converter.py
+++ snapmatic-converter.py
@@ -5,17 +5,17 @@ home_directory = os.path.join(str(Path.home()) + "\\Saved Games\\CitizenFX\\GTA5
 
 for count, pgta in enumerate(os.listdir(home_directory)):    
     if pgta.startswith("PGTA"):
-        print(pgta)
-
-        photo = open(os.path.join(home_directory, pgta), "rb")
-    
-        image_data = bytearray()
-    
-        for i in range(292, os.path.getsize(os.path.join(home_directory, pgta))):
-            photo.seek(i)
-            image_data += photo.read(1)
+        if os.path.exists(pgta + ".jpg") == False:
+            print(pgta)
+            photo = open(os.path.join(home_directory, pgta), "rb")
         
-        out_photo = open(pgta + ".jpg", "wb")
-        out_photo = out_photo.write(image_data)
-        photo.close()
+            image_data = bytearray()
+        
+            for i in range(292, os.path.getsize(os.path.join(home_directory, pgta))):
+                photo.seek(i)
+                image_data += photo.read(1)
+            
+            out_photo = open(pgta + ".jpg", "wb")
+            out_photo = out_photo.write(image_data)
+            photo.close()
 print("Todas las imagenes fueron convertidas")
